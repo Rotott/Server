d478341a67d4ef964e16ca42057749a77dbfe54a
Rotott <161693601+Rotott@users.noreply.github.com>
2024-03-26 11:45:47 +0100

    upload new version file

---
+++ Server/Project/src/model/Ingredient.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Ingredient {
+    private int id;
+    private String name;
+    private int idRecipe;
+    private String measure;
+    public Ingredient() {
+
+    }
+}
